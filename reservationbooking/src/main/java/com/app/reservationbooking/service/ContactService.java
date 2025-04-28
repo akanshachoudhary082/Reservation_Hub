03ee26a3d8345a6b6ec2b61fdabdb6e04317ce59
Akansha Choudhary <103350630+akanshachoudhary082@users.noreply.github.com>
2025-04-28 16:43:32 +0530

    Add frontend/.env to .gitignore

--- reservationbooking/src/main/java/com/app/reservationbooking/service/ContactService.java
+++
@@ -1,8 +0,0 @@
-//package com.app.reservationbooking.service;
-//
-//
-//import com.app.reservationbooking.dto.ContactRequestDTO;
-//
-//public interface ContactService {
-//    void sendEmail(ContactRequestDTO contactRequestDTO);
-//}
